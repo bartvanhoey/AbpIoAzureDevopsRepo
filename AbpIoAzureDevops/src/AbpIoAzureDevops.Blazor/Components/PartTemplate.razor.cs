5719c821a01c984927525361457d1ae88ddeb95e
bartvanhoey <bartvanhoey@hotmail.com>
2020-10-12 06:25:11 +0200

    end_part3

--- AbpIoAzureDevops/src/AbpIoAzureDevops.Blazor/Components/PartTemplate.razor.cs
+++ AbpIoAzureDevops/src/AbpIoAzureDevops.Blazor/Components/PartTemplate.razor.cs
@@ -1,22 +1,57 @@
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Volo.Abp;
 
 namespace AbpIoAzureDevops.Blazor.Components
 {
-    public class PartTemplateBase : ComponentBase
-    {
-        [Inject] IJSRuntime JSRuntime { get; set; }
-        [Parameter] public string Title { get; set; }
-        [Parameter] public string Href { get; set; }
-        [Parameter] public string PrevHref { get; set; }
-        [Parameter] public string NextHref { get; set; }
-        [Parameter] public string PageIdentifier { get; set; }
-        [Parameter] public RenderFragment Part { get; set; }
+  public class PartTemplateBase : ComponentBase
+  {
+    [Inject] IJSRuntime JSRuntime { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
+    [Parameter] public string Title { get; set; }
+    [Parameter] public string Href { get; set; }
+    [Parameter] public string PrevHref { get; set; }
+    [Parameter] public string NextHref { get; set; }
+    [Parameter] public string PageIdentifier { get; set; }
+    [Parameter] public RenderFragment Part { get; set; }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            await JSRuntime.InvokeVoidAsync("Prism.highlightAll");
-        }
+    protected int PreviousPage = -1;
+    protected int CurrentPage = -1;
+    protected int NextPage = -1;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+      await JSRuntime.InvokeVoidAsync("Prism.highlightAll");
     }
+
+
+
+
+    protected override void OnParametersSet()
+    {
+      if (!string.IsNullOrWhiteSpace(PrevHref))
+      {
+        int.TryParse(Regex.Match(PrevHref, @"\d+").Value, out var iPreviousPage);
+        PreviousPage = iPreviousPage;
+      }
+
+      if (!string.IsNullOrWhiteSpace(Href))
+      {
+        int.TryParse(Regex.Match(Href, @"\d+").Value, out var iCurrentPage);
+        CurrentPage = iCurrentPage;
+      }
+
+      if (!string.IsNullOrWhiteSpace(NextHref))
+      {
+        int.TryParse(Regex.Match(NextHref, @"\d+").Value, out var iNextPage);
+        NextPage = iNextPage;
+      }
+    }
+
+
+    protected void NavigateToPage(string hRef) => NavigationManager.NavigateTo(hRef);
+
+  }
 }
--- AbpIoAzureDevops/src/AbpIoAzureDevops.Blazor/Constants.cs
+++ AbpIoAzureDevops/src/AbpIoAzureDevops.Blazor/Constants.cs
@@ -6,7 +6,7 @@ namespace AbpIoAzureDevops.Blazor
     {
       public const string Title = "Part 1: Create a new GitHub Repository";
       public const string Href = "part1";
-      public const string PageIdentifier = "001";
+      public const string PageIdentifier = "1";
       public const string PrevHref = "";
       public const string NextHref = "part2";
     }
@@ -15,9 +15,18 @@ namespace AbpIoAzureDevops.Blazor
     {
       public const string Title = "Part 2: Create a new abp.io application";
       public const string Href = "part2";
-      public const string PageIdentifier = "002";
+      public const string PageIdentifier = "2";
       public const string PrevHref = "part1";
       public const string NextHref = "part3";
+    }
+    
+    public static class Part3
+    {
+      public const string Title = "Part 3: Create an SQL Database on Azure and change ConnectionString in appsettings.json file";
+      public const string Href = "part3";
+      public const string PageIdentifier = "3";
+      public const string PrevHref = "part2";
+      public const string NextHref = "part4";
     }
   }
 }
