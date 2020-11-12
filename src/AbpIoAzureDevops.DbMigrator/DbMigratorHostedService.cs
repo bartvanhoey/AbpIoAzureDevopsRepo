20bf7d7a97520ca48e0f596efb1b17e5ef3b39dc
bartvanhoey <bartvanhoey@hotmail.com>
2020-11-12 23:22:53 +0100

    update part3

--- src/AbpIoAzureDevops.DbMigrator/DbMigratorHostedService.cs
+++ src/AbpIoAzureDevops.DbMigrator/DbMigratorHostedService.cs
@@ -5,39 +5,61 @@ using Microsoft.Extensions.Hosting;
 using AbpIoAzureDevops.Data;
 using Serilog;
 using Volo.Abp;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace AbpIoAzureDevops.DbMigrator
 {
-    public class DbMigratorHostedService : IHostedService
+  public class DbMigratorHostedService : IHostedService
+  {
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+    public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime)
     {
-        private readonly IHostApplicationLifetime _hostApplicationLifetime;
-
-        public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime)
-        {
-            _hostApplicationLifetime = hostApplicationLifetime;
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            using (var application = AbpApplicationFactory.Create<AbpIoAzureDevopsDbMigratorModule>(options =>
-            {
-                options.UseAutofac();
-                options.Services.AddLogging(c => c.AddSerilog());
-            }))
-            {
-                application.Initialize();
-
-                await application
-                    .ServiceProvider
-                    .GetRequiredService<AbpIoAzureDevopsDbMigrationService>()
-                    .MigrateAsync();
-
-                application.Shutdown();
-
-                _hostApplicationLifetime.StopApplication();
-            }
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+      _hostApplicationLifetime = hostApplicationLifetime;
     }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+      using (var application = AbpApplicationFactory.Create<AbpIoAzureDevopsDbMigratorModule>(options =>
+      {
+        options.UseAutofac();
+        options.Services.AddLogging(c => c.AddSerilog());
+
+        // Add this line of code to make it possible read from appsettings.Staging.json
+        options.Services.ReplaceConfiguration(BuildConfiguration());
+
+      }))
+      {
+        application.Initialize();
+
+        await application
+            .ServiceProvider
+            .GetRequiredService<AbpIoAzureDevopsDbMigrationService>()
+            .MigrateAsync();
+
+        application.Shutdown();
+
+        _hostApplicationLifetime.StopApplication();
+      }
+    }
+
+    private static IConfiguration BuildConfiguration()
+    {
+      var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+      // Extra code block to make it possible to read from appsettings.Staging.json
+      var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+      if (environmentName == "Staging")
+      {
+        configurationBuilder.AddJsonFile($"appsettings.{environmentName}.json", true);
+      }
+
+      return configurationBuilder
+          .AddEnvironmentVariables()
+          .Build();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+  }
 }
--- src/AbpIoAzureDevops.DbMigrator/Program.cs
+++ src/AbpIoAzureDevops.DbMigrator/Program.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -8,33 +9,35 @@ using Serilog.Events;
 
 namespace AbpIoAzureDevops.DbMigrator
 {
-    class Program
+  class Program
+  {
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning)
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Information()
+          .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+          .MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning)
 #if DEBUG
                 .MinimumLevel.Override("AbpIoAzureDevops", LogEventLevel.Debug)
 #else
                 .MinimumLevel.Override("AbpIoAzureDevops", LogEventLevel.Information)
 #endif
                 .Enrich.FromLogContext()
-                .WriteTo.Async(c => c.File("Logs/logs.txt"))
-                .WriteTo.Async(c => c.Console())
-                .CreateLogger();
+          .WriteTo.Async(c => c.File("Logs/logs.txt"))
+          .WriteTo.Async(c => c.Console())
+          .CreateLogger();
 
-            await CreateHostBuilder(args).RunConsoleAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging((context, logging) => logging.ClearProviders())
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<DbMigratorHostedService>();
-                });
+      await CreateHostBuilder(args).RunConsoleAsync();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+
+        Host.CreateDefaultBuilder(args)
+            .ConfigureLogging((context, logging) => logging.ClearProviders())
+             .ConfigureServices((hostContext, services) =>
+            {
+              services.AddHostedService<DbMigratorHostedService>();
+            });
+  }
 }
